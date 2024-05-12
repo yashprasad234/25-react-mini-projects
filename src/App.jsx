e4678e2eced9deb085345b94889758be4f1cd769
Yash Prasad <64632275+yashprasad234@users.noreply.github.com>
2024-05-12 18:38:54 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Accordion from "./components/accordion/index";
+import RandomColor from "./components/random-color";
+
+function App() {
+  return (
+    <>
+      {/* Accordion component */}
+      {/* <Accordion /> */}
+      {/* Random-Color component */}
+      <RandomColor />
+    </>
+  );
+}
+
+export default App;
